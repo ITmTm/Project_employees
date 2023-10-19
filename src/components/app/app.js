333479fd063d9ca0b57f30139285b695daa05cf4
Timur <Stafr95@mail.ru>
2023-10-19 12:31:53 +0500

    creating an array for a list of elements

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from "../employees-add-form/employees-add-form";
 import './app.css';
 
 function App() {
+
+	const data = [
+		{name: 'Anthony S.', salary: 1800, increase: false},
+		{name: 'Timur M.', salary: 2000, increase: true},
+		{name: 'Vildan F.', salary: 2300, increase: false},
+	];
+
 	return (
 		<div className='app'>
 			<AppInfo/>
@@ -16,7 +23,7 @@ function App() {
 				<AppFilter />
 			</div>
 
-			<EmployeesList />
+			<EmployeesList data={data} />
 			<EmployeesAddForm />
 		</div>
 	);
