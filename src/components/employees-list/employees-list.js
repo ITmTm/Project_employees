c8970ae5b7d0c264d2097fa64ea91b524500777f
Timur <Stafr95@mail.ru>
2023-10-19 12:31:01 +0500

    forming elements using the spread operator

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,12 +1,17 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+	const elements = data.map(item => {
+		return (
+			<EmployeesListItem {...item}/>
+		)
+	})
+
 	return (
 		<ul className='app-list list-group'>
-			<EmployeesListItem name='Anthony S.' salary={1500}/>
-			<EmployeesListItem name='Timur M.' salary={2000}/>
-			<EmployeesListItem name='Vildan F.' salary={2300}/>
+			{elements}
 		</ul>
 	);
 }
