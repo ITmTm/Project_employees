845bfba92626f50c816dabdebe932fe415cf13a0
Timur <Stafr95@mail.ru>
2023-10-19 11:53:07 +0500

    Using destructuring

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -4,9 +4,9 @@ import './employees-list.css';
 const EmployeesList = () => {
 	return (
 		<ul className='app-list list-group'>
-			<EmployeesListItem />
-			<EmployeesListItem />
-			<EmployeesListItem />
+			<EmployeesListItem name='Anthony S.' salary={1500}/>
+			<EmployeesListItem name='Timur M.' salary={2000}/>
+			<EmployeesListItem name='Vildan F.' salary={2300}/>
 		</ul>
 	);
 }
