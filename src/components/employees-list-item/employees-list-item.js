e40814dfeac647787509f8da2a2801d17a45ef9e
Timur <Stafr95@mail.ru>
2023-10-23 17:03:23 +0500

    adding like

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,11 +1,14 @@
-import './employees-list-item.css';
 import {Component} from "react";
+
+import './employees-list-item.css';
+
 
 class EmployeesListItem extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			increase: false,
+			like: false
 		}
 	}
 
@@ -14,18 +17,30 @@ class EmployeesListItem extends Component {
 			increase: !increase
 		}))
 	}
+
+	onLike = () => {
+		this.setState(({like}) => ({
+			like: !like
+		}))
+	}
 	render() {
 		const {name, salary} = this.props;
-		const {increase} = this.state;
+		const {increase, like} = this.state;
 
 		let classNames = 'list-group-item d-flex justify-content-between';
+
 		if (increase) {
 			classNames += ' increase';
 		}
 
+		if (like) {
+			classNames += ' like';
+		}
+
 		return (
 			<li className={classNames}>
-				<span className='list-group-item-label'>{name}</span>
+				<span
+					className='list-group-item-label' onClick={this.onLike}>{name}</span>
 				<input type="text"
 					   className='list-group-item-input'
 					   defaultValue={salary + ' $'}
@@ -43,11 +58,10 @@ class EmployeesListItem extends Component {
 						className='btn-trash btn-sm'>
 						<i className='fas fa-trash'></i>
 					</button>
-
 					<i className='fas fa-star'></i>
 				</div>
 			</li>
-		);
+		)
 	}
 }
 
