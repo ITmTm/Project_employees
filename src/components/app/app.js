b841c2ddd42c784f3f9e14f7147622f79a15c69e
Timur <Stafr95@mail.ru>
2023-10-31 17:44:53 +0500

    implementation of search and filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ class App extends Component {
 				{name: 'Timur M.', salary: 2000, increase: true, rise: false, id: 2},
 				{name: 'Vildan F.', salary: 2300, increase: false, rise: false, id: 3},
 			],
-			term: ''
+			term: '',
+			filter: 'all'
 		}
 		this.maxId = 4;
 	}
@@ -107,11 +108,26 @@ class App extends Component {
 		this.setState({term: term});
 	}
 
+	filterPost = (items, filter) => {
+		switch (filter) {
+			case 'rise':
+				return items.filter(item => item.rise);
+			case 'moreThen1000':
+				return items.filter(item => item.salary > 1000);
+			default:
+				return items;
+		}
+	}
+
+	onFilterSelect = (filter) => {
+		this.setState({filter: filter});
+	}
+
 	render() {
-		const {data, term} = this.state;
+		const {data, term, filter} = this.state;
 		const employees = this.state.data.length;
 		const increased = this.state.data.filter(item => item.increase).length;
-		const visibleData = this.searchEmp(data, term);
+		const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
 		return (
 			<div className='app'>
@@ -125,7 +141,10 @@ class App extends Component {
 					<SearchPanel
 						onUpdateSearch={this.onUpdateSearch}
 					/>
-					<AppFilter />
+					<AppFilter
+						filter={filter}
+						onFilterSelect={this.onFilterSelect}
+					/>
 				</div>
 
 				<EmployeesList
