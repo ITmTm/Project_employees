94b8ff615ed134f46013f036a8d39fbec1ec7112
Timur <Stafr95@mail.ru>
2023-10-31 16:55:28 +0500

    implementation of search and filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ class App extends Component {
 				{name: 'Anthony S.', salary: 1800, increase: false, rise: true, id: 1},
 				{name: 'Timur M.', salary: 2000, increase: true, rise: false, id: 2},
 				{name: 'Vildan F.', salary: 2300, increase: false, rise: false, id: 3},
-			]
+			],
+			term: ''
 		}
 		this.maxId = 4;
 	}
@@ -92,9 +93,25 @@ class App extends Component {
 		}))
 	}
 
+	searchEmp = (items, term) => {
+		if (term.length === 0) {
+			return items;
+		}
+
+		return items.filter(item => {
+			return item.name.indexOf(term) > -1
+		})
+	}
+
+	onUpdateSearch = (term) => {
+		this.setState({term: term});
+	}
+
 	render() {
+		const {data, term} = this.state;
 		const employees = this.state.data.length;
 		const increased = this.state.data.filter(item => item.increase).length;
+		const visibleData = this.searchEmp(data, term);
 
 		return (
 			<div className='app'>
@@ -105,12 +122,14 @@ class App extends Component {
 				/>
 
 				<div className='search-panel'>
-					<SearchPanel />
+					<SearchPanel
+						onUpdateSearch={this.onUpdateSearch}
+					/>
 					<AppFilter />
 				</div>
 
 				<EmployeesList
-					data={this.state.data}
+					data={visibleData}
 					onDelete={this.deleteItems}
 					onToggleProp={this.onToggleProp}
 				/>
